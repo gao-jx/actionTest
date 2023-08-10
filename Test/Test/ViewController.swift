632d433c1fe88a5149a3e8a02f79dea0e5758fc9
Jianxun Gao <gaojianxun@itiger.com>
2023-08-10 16:43:14 +0800

    initial

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by gjx on 2023/8/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
